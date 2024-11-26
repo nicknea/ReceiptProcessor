afd5bd44644a2f04364026001e3e639599e35d89
nicknea <nicknea@hotmail.com>
2024-11-25 20:51:25 -0600

    Processing receipt done

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"regexp"
 	sl "slices"
+	"strconv"
 	str "strings"
 )
 
@@ -40,16 +42,51 @@ func processTotal(t string, p *int) {
 	}
 }
 
-func processItemCount(i []Item, p *int) {
+func processItemsCount(i []Item, p *int) {
 	if len(i)%2 >= 0 {
 		*p += (len(i) / 2) * 5
 	}
 }
+
+func processItem(i Item, p *int) {
+	if len(str.TrimSpace(i.ShortDescription))%3 == 0 {
+		pf, _ := strconv.ParseFloat(i.Price, 32)
+		*p += int(math.Ceil((pf * 0.2)))
+	}
+}
+
+func processItems(i []Item, p *int) {
+	processItemsCount(i, p)
+	for _, item := range i {
+		processItem(item, p)
+	}
+}
+
+func processPurchaseDate(d string, p *int) {
+	date, _ := strconv.ParseInt(d[len(d)-2:], 10, 32)
+	if date%2 != 0 {
+		*p += 6
+	}
+}
+
+func processPurchaseTime(t string, p *int) {
+	log.Println(strconv.ParseFloat(t[:2], 32))
+	hour, _ := strconv.ParseFloat(t[:2], 32)
+	minute, _ := strconv.ParseFloat(t[3:], 32)
+	time := hour + (minute / 100.00)
+
+	if time > 14 && time < 16 {
+		*p += 10
+	}
+}
+
 func process(r Receipt) {
 	p := 0
 	processRetailer(r.Retailer, &p)
 	processTotal(r.Total, &p)
-	processItemCount(r.Items, &p)
+	processItems(r.Items, &p)
+	processPurchaseDate(r.PurchaseDate, &p)
+	processPurchaseTime(r.PurchaseTime, &p)
 	log.Println(r, p)
 }
 
--- main_test.go
+++ main_test.go
@@ -15,7 +15,7 @@ func TestProcessRetailer(t *testing.T) {
 }
 
 func TestProcessRetailerNonAlpha(t *testing.T) {
-	r := Receipt{Retailer: "abcdef123456@#$#@$"}
+	r := Receipt{Retailer: "abcdef123456!@#$%^&"}
 	p := 0
 	processRetailer(r.Retailer, &p)
 
@@ -57,44 +57,44 @@ func TestProcessTotalNotRoundNotMultiple(t *testing.T) {
 	}
 }
 
-func TestProcessItemCountOne(t *testing.T) {
+func TestProcessItemsCountOne(t *testing.T) {
 	i1 := Item{ShortDescription: "Item 1", Price: "1.00"}
 	r := Receipt{Items: []Item{i1}}
 	p := 0
 
-	processItemCount(r.Items, &p)
+	processItemsCount(r.Items, &p)
 
 	if p != 0 {
 		t.Fatalf("expected 0, got %v", p)
 	}
 }
-func TestProcessItemCountTwo(t *testing.T) {
+func TestProcessItemsCountTwo(t *testing.T) {
 	i1 := Item{ShortDescription: "Item 1", Price: "1.00"}
 	i2 := Item{ShortDescription: "Item 2", Price: "2.00"}
 	r := Receipt{Items: []Item{i1, i2}}
 	p := 0
 
-	processItemCount(r.Items, &p)
+	processItemsCount(r.Items, &p)
 
 	if p != 5 {
 		t.Fatalf("expected 5, got %v", p)
 	}
 }
-func TestProcessItemCountThree(t *testing.T) {
+func TestProcessItemsCountThree(t *testing.T) {
 	i1 := Item{ShortDescription: "Item 1", Price: "1.00"}
 	i2 := Item{ShortDescription: "Item 2", Price: "2.00"}
 	i3 := Item{ShortDescription: "Item 3", Price: "3.00"}
 	r := Receipt{Items: []Item{i1, i2, i3}}
 	p := 0
 
-	processItemCount(r.Items, &p)
+	processItemsCount(r.Items, &p)
 
 	if p != 5 {
 		t.Fatalf("expected 5, got %v", p)
 	}
 }
 
-func TestProcessItemCountFour(t *testing.T) {
+func TestProcessItemsCountFour(t *testing.T) {
 	i1 := Item{ShortDescription: "Item 1", Price: "1.00"}
 	i2 := Item{ShortDescription: "Item 2", Price: "2.00"}
 	i3 := Item{ShortDescription: "Item 3", Price: "3.00"}
@@ -102,9 +102,99 @@ func TestProcessItemCountFour(t *testing.T) {
 	r := Receipt{Items: []Item{i1, i2, i3, i4}}
 	p := 0
 
-	processItemCount(r.Items, &p)
+	processItemsCount(r.Items, &p)
 
 	if p != 10 {
 		t.Fatalf("expected 10, got %v", p)
+	}
+}
+
+func TestProcessItemMultiple3(t *testing.T) {
+	i := Item{ShortDescription: "123456 89", Price: "12.25"}
+	p := 0
+	processItem(i, &p)
+
+	if p != 3 {
+		t.Fatalf("expected 3, got %v", p)
+	}
+}
+
+func TestProcessItemNotMultiple3(t *testing.T) {
+	i := Item{ShortDescription: "123456 8", Price: "12.25"}
+	p := 0
+	processItem(i, &p)
+
+	if p != 0 {
+		t.Fatalf("expected 0, got %v", p)
+	}
+}
+
+func TestProcessItems(t *testing.T) {
+	i1 := Item{ShortDescription: "123456", Price: "10.00"}   //2
+	i2 := Item{ShortDescription: "12345", Price: "2.00"}     //0
+	i3 := Item{ShortDescription: "123456789", Price: "6.00"} //1.2 => 2
+	i4 := Item{ShortDescription: "12", Price: "4.00"}
+	r := Receipt{Items: []Item{i1, i2, i3, i4}}
+	p := 0
+
+	processItems(r.Items, &p)
+
+	if p != 14 {
+		t.Fatalf("expected 14, got %v", p)
+	}
+
+}
+
+func TestProcessPurchaseDateOdd(t *testing.T) {
+	r := Receipt{PurchaseDate: "2024-01-11"}
+	p := 0
+
+	processPurchaseDate(r.PurchaseDate, &p)
+
+	if p != 6 {
+		t.Fatalf("expected 6, got %v", p)
+	}
+}
+
+func TestProcessPurchaseDateEven(t *testing.T) {
+	r := Receipt{PurchaseDate: "2024-01-02"}
+	p := 0
+
+	processPurchaseDate(r.PurchaseDate, &p)
+
+	if p != 0 {
+		t.Fatalf("expected 0, got %v", p)
+	}
+}
+func TestProcessPurchaseTimeIn(t *testing.T) {
+	r := Receipt{PurchaseTime: "14:01"}
+	p := 0
+
+	processPurchaseTime(r.PurchaseTime, &p)
+
+	if p != 10 {
+		t.Fatalf("expected 10, got %v", p)
+	}
+}
+
+func TestProcessPurchaseTimeBefore(t *testing.T) {
+	r := Receipt{PurchaseTime: "13:59"}
+	p := 0
+
+	processPurchaseTime(r.PurchaseTime, &p)
+
+	if p != 0 {
+		t.Fatalf("expected 0, got %v", p)
+	}
+}
+
+func TestProcessPurchaseTimeAfter(t *testing.T) {
+	r := Receipt{PurchaseTime: "16:01"}
+	p := 0
+
+	processPurchaseTime(r.PurchaseTime, &p)
+
+	if p != 0 {
+		t.Fatalf("expected 0, got %v", p)
 	}
 }
